239d3f11eff410ac751458b1a1ecca668834a01e
Roman Kananovich <roman.kananovich@gmail.com>
2019-06-17 23:11:25 +0300

    initial

---
+++ Task3/Task3/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Task3
+//
+//  Created by Roma on 6/17/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Task3/Task3/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Task3
+//
+//  Created by Roma on 6/17/19.
+//  Copyright © 2019 Roman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
